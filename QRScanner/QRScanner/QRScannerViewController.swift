1085e21e1f210a11abb529c8ccf6f5b5dd345121
Abhilash KM <abhilash.a.km@rakuten.com>
2019-03-11 10:48:54 +0530

    Adding the project files

---
+++ QRScanner/QRScanner/QRScannerViewController.swift
@@ -0,0 +1,74 @@
+//
+//  QRScannerViewController.swift
+//  QRCodeReader
+//
+//  Created by KM, Abhilash a on 08/03/19.
+//  Copyright © 2019 KM, Abhilash. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerViewController: UIViewController {
+    
+    @IBOutlet weak var scannerView: QRScannerView! {
+        didSet {
+            scannerView.delegate = self
+        }
+    }
+    @IBOutlet weak var scanButton: UIButton! {
+        didSet {
+            scanButton.setTitle("STOP", for: .normal)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if !scannerView.isRunning {
+            scannerView.startScanning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if !scannerView.isRunning {
+            scannerView.stopScanning()
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    @IBAction func scanButtonAction(_ sender: UIButton) {
+        scannerView.isRunning ? scannerView.stopScanning() : scannerView.startScanning()
+        let buttonTitle = scannerView.isRunning ? "STOP" : "SCAN"
+        sender.setTitle(buttonTitle, for: .normal)
+    }
+}
+
+
+extension QRScannerViewController: QRScannerViewDelegate {
+    func qrScanningDidStop() {
+        let buttonTitle = scannerView.isRunning ? "STOP" : "SCAN"
+        scanButton.setTitle(buttonTitle, for: .normal)
+    }
+    
+    func qrScanningDidFail() {
+        print("Failed")
+    }
+    
+    func qrScanningSucceededWithCode(_ str: String?) {
+        print(str)
+    }
+    
+    
+}
---
+++ QRScanner/QRScanner/View/QRScannerView.swift
@@ -0,0 +1,109 @@
+//
+//  VideoScannerView.swift
+//  QRCodeReader
+//
+//  Created by KM, Abhilash a on 08/03/19.
+//  Copyright © 2019 KM, Abhilash. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+protocol QRScannerViewDelegate: class {
+    func qrScanningDidFail()
+    func qrScanningSucceededWithCode(_ str: String?)
+    func qrScanningDidStop()
+}
+
+class QRScannerView: UIView {
+    
+    var captureSession: AVCaptureSession?
+    weak var delegate: QRScannerViewDelegate?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        doInitialSetup()
+    }
+    override class var layerClass: AnyClass  {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    override var layer: AVCaptureVideoPreviewLayer {
+        return super.layer as! AVCaptureVideoPreviewLayer
+    }
+}
+extension QRScannerView {
+    
+    var isRunning: Bool {
+        return captureSession?.isRunning ?? false
+    }
+    func startScanning() {
+       captureSession?.startRunning()
+    }
+    func stopScanning() {
+        captureSession?.stopRunning()
+        delegate?.qrScanningDidStop()
+    }
+    private func doInitialSetup() {
+        clipsToBounds = true
+        captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession?.canAddInput(videoInput) ?? false) {
+            captureSession?.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession?.canAddOutput(metadataOutput) ?? false) {
+            captureSession?.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr, .ean8, .ean13, .pdf417]
+        } else {
+            failed()
+            return
+        }
+        
+        self.layer.session = captureSession
+        self.layer.videoGravity = .resizeAspectFill
+        
+        captureSession?.startRunning()
+    }
+    func failed() {
+        delegate?.qrScanningDidFail()
+        captureSession = nil
+    }
+    
+    func found(code: String) {
+        delegate?.qrScanningSucceededWithCode(code)
+    }
+    
+}
+
+extension QRScannerView: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        stopScanning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+        
+        //        dismiss(animated: true)
+    }
+    
+}
