ba00e69d15753a172d0f43fb1bf3a416c8a43e05
Francisco Gindre <francisco.gindre@gmail.com>
2020-02-04 12:19:14 -0300

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "QRScanner",
+    name: "TinyQRScanner",
 
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "QRScanner",
-            targets: ["QRScanner"]),
+            name: "TinyQRScanner",
+            targets: ["TinyQRScanner"]),
     ],
     dependencies: [     
     ],
     targets: [
 
         .target(
-            name: "QRScanner",
+            name: "TinyQRScanner",
             dependencies: []),
         .testTarget(
-            name: "QRScannerTests",
-            dependencies: ["QRScanner"]),
+            name: "TinyQRScanner",
+            dependencies: ["TinyQRScanner"]),
     ],
     swiftLanguageVersions: [.v5]
 )
